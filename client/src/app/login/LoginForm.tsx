023782dfb0b0b7787f84e9d0f3cdede70b1b0387
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2024-03-26 21:54:48 +0100

    install reacthookform/zod

---
+++ client/src/app/login/LoginForm.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import LabelInput from "@/components/ui/LabelInput";
+import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+
+const loginFormSchema = z.object({
+  email: z.string().email().trim().min(1),
+  password: z.string().min(1),
+});
+
+type LoginFormSchemaType = z.infer<typeof loginFormSchema>;
+
+export default function LoginForm() {
+  const form = useForm<LoginFormSchemaType>({
+    resolver: zodResolver(loginFormSchema),
+  });
+
+  const { handleSubmit } = form;
+
+  function onSubmit() {}
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto max-w-[476px] space-y-10 md:rounded-lg md:bg-white md:p-10 md:shadow-sm"
+      >
+        <header className="space-y-2">
+          <h1 className="text-heading-m">Login</h1>
+          <p className="text-body-m">
+            Add your details below to get back into the app
+          </p>
+        </header>
+        <div className="space-y-6">
+          <LabelInput
+            label="Email address"
+            value="email"
+            type="email"
+            placeholder="e.g. alex@email.com"
+            icon="/images/icon-email.svg"
+          />
+          <LabelInput
+            label="Password"
+            value="password"
+            type="password"
+            placeholder="Enter your password"
+            icon="/images/icon-password.svg"
+          />
+          <Button variant={"default"}>Login</Button>
+        </div>
+        <footer className="text-body-m flex w-full flex-col items-center justify-center md:flex-row md:gap-1">
+          <p>Don&apos;t have an account?</p>
+          <Link
+            className="text-purple hover:text-purpleHover  transition-colors"
+            href={"/register"}
+          >
+            Create account
+          </Link>
+        </footer>
+      </form>
+    </Form>
+  );
+}
--- client/src/components/ui/LabelInput.tsx
+++ client/src/components/ui/LabelInput.tsx
@@ -1,19 +1,41 @@
+"use client";
 import React from "react";
-import { Label } from "./label";
+import { useFormContext } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./form";
 import { Input } from "./input";
+import { Label } from "./label";
 
 type Props = {
   label: string;
+  value: string;
   type: React.HTMLInputTypeAttribute;
   placeholder: string;
   icon: string;
 };
 
-export default function LabelInput({ label, type, placeholder }: Props) {
+export default function LabelInput({ label, value, type, placeholder }: Props) {
+  const { control } = useFormContext();
   return (
-    <div className="">
-      <Label className="text-body-s">{label}</Label>
-      <Input type={type} placeholder={placeholder} />
-    </div>
+    <FormField
+      name={value}
+      control={control}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="text-body-s">{label}</FormLabel>
+          <FormControl>
+            <div className="relative">
+              <Input type={type} placeholder={placeholder} {...field} />
+              <FormMessage className="text-body-s absolute right-4 top-1/2 -translate-y-1/2 font-thin text-red" />
+            </div>
+          </FormControl>
+        </FormItem>
+      )}
+    />
   );
 }
